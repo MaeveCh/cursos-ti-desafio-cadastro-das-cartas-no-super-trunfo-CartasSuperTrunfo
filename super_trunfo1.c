bb0db201fb3c79a3b18a28d520cf79bff33e01b1
MaeveCh <victoriacheregatiamenta@gmail.com>
2025-04-20 17:49:12 +0000

    printf anagrama

--- super_trunfo1.c
+++ super_trunfo1.c
@@ -5,20 +5,21 @@ int main(){
      int populacao, turismo;
      float pib, area;
      char inicial;
-     char carta, nome[20];
+     char nome[20];
+     char carta[20];
      
 
     printf("Digite o código da carta: \n");
      // A letra do Estado seguida de um número de 01 À 04, ex: A01
      scanf("%s", &carta);
 
-     printf("Digite o nome da Cidade: \n");
+     printf("Digite o nome da Cidade:  \n");
      // Use um nome próprio, ex: Niterói
      scanf("%s", &nome);
 
      printf("Digite um anagrama: \n");
      // Somente uma Letra/Anagrama, que seja de A a H, para nomear o Estado, ex: Estado RJ - A
-     scanf("%c", &inicial);
+     scanf("%s", &inicial);
 
      printf("Digite a quantidade de habitantes: \n");
      // Quantas pessoas moram neste território, ex: 481.749
@@ -26,7 +27,7 @@ int main(){
 
      printf("Digite a quantidade de pontos turísticos na Cidade: \n");
     // Pontos turísticos representado por 'turismo' é um local frequentemente vistado por turistas, ex: 10
-     scanf("%d", &turismo);
+     scanf("%d\n", &turismo);
 
      printf("Digite o valor Produto Interno Bruto da Cidade: \n");
      //Produto Interno Bruto é o PIB, ele indica a riqueza da Cidade num período definido, ex: 66.349.000.000
@@ -36,16 +37,14 @@ int main(){
      // A área em km^2 é a dimensão do território, ou seja, o tamanho dele, ex: 133.757
      scanf("%f\n", &area);
 
-
-     // Finalização do código Carta 1
       
-     printf("O nome da cidade é: %s - O código da carta é: %s\n", carta, nome);
+     printf("O nome da cidade é: %s - O código da carta é: %s\n", nome, carta);
 
-     printf("O anagrama é: %c\n", inicial);
+     printf("O anagrama é: %s\n", inicial);
 
-     printf("O número de habitantes é: %d - O número de pontos turísticos é: %d", populacao, turismo);
+     printf("O número de habitantes é: %d - O número de pontos turísticos é: %d\n", populacao, turismo);
 
-     printf("O PIB da cidade é: %f - A área da cidade é: %f", pib, area);
+     printf("O PIB da cidade é: %f - A área da cidade é: %f\n", pib, area);
 
 
     return 0;
