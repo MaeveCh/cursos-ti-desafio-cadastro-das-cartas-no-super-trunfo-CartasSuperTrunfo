22313b259e7a4148896a5e7a1d69f26f1106ea77
MaeveCh <victoriacheregatiamenta@gmail.com>
2025-04-20 15:52:27 +0000

    printf

--- super_trunfo.c
+++ super_trunfo.c
@@ -22,18 +22,19 @@ int main(){
      scanf("%c", &estado);
 
      printf("Digite o número de habitantes: \n");
+     // Quantas pessoas moram neste território, ex: 481.749
     scanf("%d", &populacao);
 
      printf("Digite o número de pontos turísticos: \n");
-    // Pontos turísticos representado por 'pontost' é um local frequentemente vistado por turistas.
+    // Pontos turísticos representado por 'pontost' é um local frequentemente vistado por turistas, ex: 10
     scanf("%d", &pontost);
 
      printf("Digite o Produto Interno Bruto da Cidade: \n");
-     //Produto Interno Bruto é o PIB, ele indica a riqueza da Cidade num período definido.
+     //Produto Interno Bruto é o PIB, ele indica a riqueza da Cidade num período definido, ex: 66.349.000.000
      scanf("%f", &pib);
 
      printf("Digite a área em quilômetros quadrados 'km^2': \n");
-     // A área em km^2 é a dimensão do território, ou seja, o tamanho dele. 
+     // A área em km^2 é a dimensão do território, ou seja, o tamanho dele, ex: 133.757
      scanf("%f", &area);
 
 
